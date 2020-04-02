3bd24c6204b0431648bd4b1f3a137af2ccc7f6f6
Michal Jurkowski <1812653+mjurkowski@users.noreply.github.com>
2020-04-02 20:42:08 +0200

    Code refactor / bug fixing (#2)
    * code refactor
    
    * code formatting

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,81 +1,93 @@
-const getChartTraces = () => {
-  const leaderboard = [];
-  const { place, dydx } = getOptions();
+class Chart {
+  selector = '#chart';
 
-  if (data.activeLeaderboard.length) {
-    leaderboard.push(...data.activeLeaderboard);
-  } else {
-    leaderboard.push(...getFilteredByPlaceLeaderborardsKeys().slice(0, 3))
+  constructor(app, submissions) {
+    this.app = app;
+    this.submissions = submissions;
   }
 
-  const predictions = getLeaderboardEntries(leaderboard).map(entry => {
-    const filteredPredictions = entry.predictions.filter(r => r[2] === place)
-    return {
-      x: filteredPredictions.map(r => new Date(r[0])),
-      y: filteredPredictions.map(r => parseFloat(r[1])),
-      name: entry.id,
-      line: {
-        dash: 'dot',
-        width: 2
+  getChartTraces = () => {
+    const { place, dydx } = this.app.options;
+
+    const predictions = this.submissions
+      .getSubmissionEntriesToTrace()
+      .map(entry => {
+        const filteredPredictions = entry.predictions.filter(
+          r => r[2] === place
+        );
+        return {
+          x: filteredPredictions.map(r => new Date(r[0])),
+          y: filteredPredictions.map(r => parseFloat(r[1])),
+          name: entry.id,
+          line: {
+            color: entry.color,
+            dash: 'dot',
+            width: 2
+          }
+        };
+      });
+
+    const result = [
+      ...predictions,
+      {
+        x: this.app.data.globalDeaths[0].slice(3).map(r => new Date(r)),
+        y: this.app.data.globalDeaths
+          .find(r => r[0] === place)
+          .slice(3)
+          .map(r => parseFloat(r)),
+        name: 'WHO Report',
+        mode: 'lines+markers'
       }
+    ];
+
+    if (dydx) {
+      return this.getDyDxTraces(result);
     }
-  });
 
-  const result = [
-    ...predictions,
-    {
-      x: data.globalDeaths[0].slice(3).map(r => new Date(r)),
-      y: data.globalDeaths.find(r => r[0] === place).slice(3).map(r => parseFloat(r)),
-      name: 'WHO Report',
-      mode: 'lines+markers'
-    }
-  ];
-
-  if (dydx) {
-    return getDyDxTraces(result);
-  }
-
-  return result;
-}
-
-const getDyDxTraces = (traces = []) => traces.map(trace => ({
-  ...trace,
-  y: trace.y.reduce((acc, y, index, arr) => {
-    const nextValue = arr[index + 1];
-    if (!!nextValue) {
-      acc.push(arr[index + 1] - y);
-    }
-    return acc;
-  }, [])
-}))
-
-
-const plotChat = () => {
-  const chartContainer = document.querySelector(selectors.chart);
-
-  const data = getChartTraces();
-
-  const opts = getOptions();
-
-  const layout = {
-    showlegend: true,
-    colorway,
-    legend: {
-      orientation: 'h',
-    },
-    margin: { t: 0 },
-    ...(opts.log ? {
-      yaxis: {
-        type: 'log',
-        autorange: true
-      }
-    } : {})
+    return result;
   };
 
-  const options = {
-    scrollZoom: true,
-    responsive: true
-  }
+  getDyDxTraces = (traces = []) => {
+    return traces.map(trace => ({
+      ...trace,
+      y: trace.y.reduce((acc, y, index, arr) => {
+        const nextValue = arr[index + 1];
+        if (!!nextValue) {
+          acc.push(arr[index + 1] - y);
+        }
+        return acc;
+      }, [])
+    }));
+  };
 
-  Plotly.newPlot( chartContainer, data, layout, options );
+  render = () => {
+    const chartContainer = document.querySelector(this.selector);
+
+    const data = this.getChartTraces();
+
+    const { log } = this.app.options;
+
+    const layout = {
+      showlegend: true,
+      legend: {
+        orientation: 'h'
+      },
+      margin: { t: 0 },
+      ...(log
+        ? {
+            yaxis: {
+              type: 'log',
+              autorange: true
+            }
+          }
+        : {})
+    };
+
+    const options = {
+      scrollZoom: true,
+      responsive: true
+    };
+
+    Plotly.newPlot(chartContainer, data, layout, options);
+  };
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,42 +1,34 @@
 const csv = strData => {
-  const strDelimiter = ",";
+  const strDelimiter = ',';
 
   // Create a regular expression to parse the CSV values.
   const objPattern = new RegExp(
-    (
-      // Delimiters.
-      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
+    // Delimiters.
+    '(\\' +
+      strDelimiter +
+      '|\\r?\\n|\\r|^)' +
       // Quoted fields.
-      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
+      '(?:"([^"]*(?:""[^"]*)*)"|' +
       // Standard fields.
-      "([^\"\\" + strDelimiter + "\\r\\n]*))"
-    ),
-    "gi"
+      '([^"\\' +
+      strDelimiter +
+      '\\r\\n]*))',
+    'gi'
   );
 
   const arrData = [[]];
   let arrMatches = null;
 
-  while (arrMatches = objPattern.exec(strData)) {
+  while ((arrMatches = objPattern.exec(strData))) {
     var strMatchedDelimiter = arrMatches[1];
-    if (
-      strMatchedDelimiter.length &&
-      strMatchedDelimiter !== strDelimiter
-    ) {
+    if (strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter) {
       arrData.push([]);
     }
 
     var strMatchedValue;
 
     if (arrMatches[2]) {
-
-      strMatchedValue = arrMatches[2].replace(
-        new RegExp("\"\"", "g"),
-        "\""
-      );
-
+      strMatchedValue = arrMatches[2].replace(new RegExp('""', 'g'), '"');
     } else {
       strMatchedValue = arrMatches[3];
     }
@@ -44,73 +36,9 @@ const csv = strData => {
   }
 
   return arrData;
-}
+};
 
 const formatSnakeCase = text => {
   let formattedText = text.replace(/_/g, ' ');
   return formattedText[0].toUpperCase() + formattedText.substr(1);
-}
-
-const loadAllData = () => Promise.all([
-  fetch('./leaderboard.json').then(response => response.json()),
-  fetch('./time_series_covid19_deaths_global.csv').then(response => response.text()).then(csv)
-]).then(([leaderboard, globalDeaths]) => {
-  return Promise.all(
-    Object.keys(leaderboard)
-      .map(key => 
-        fetch(`./predictions_challenge/${key}/predictions.csv`)
-          .then(response => response.text())
-          .then(csv)
-      )
-  ).then(responses => {
-    Object.keys(leaderboard).forEach((key, index) => {
-      leaderboard[key].predictions = responses[index].splice(1);
-    })
-
-    return {
-      leaderboard,
-      globalDeaths: globalDeaths.map(r => r[0] === 'WORLD' ? ['World', ...r.slice(1)] : r),
-      places: globalDeaths.map(r => r[0] === 'WORLD' ? 'World' : r[0]).slice(1).sort().filter((r, index, arr) => arr.indexOf(r) === index)
-    }
-  })
-});
-
-const getLeaderboardChildren = () => Array.from(document.querySelector(selectors.leaderboard).children)
-
-const getFilteredByPlaceLeaderborardsKeys = () => {
-  const { place } = getOptions();
-  return Object.keys(data.leaderboard)
-    .filter(key => data.leaderboard[key].scores[place])
-}
-
-const getLeaderboardEntries = (leaderboardKeys) => {
-  const { place } = getOptions();
-  return leaderboardKeys
-    .map(key => ({
-      id: key,
-      isActive: data.activeLeaderboard.includes(key),
-      ...data.leaderboard[key]
-    }))
-    .sort((a, b) => {
-      if (a.scores[place] < b.scores[place]) {
-        return 1;
-      } else if (a.scores[place] > b.scores[place]) {
-        return -1;
-      }
-
-      return 0;
-    })
-}
-
-const getOptions = () => {
-  const placeInput = document.querySelector(selectors.placeInput);
-  const placeOption = placeInput.options[placeInput.selectedIndex];
-  return {
-    place: placeOption ? placeOption.value : defaultSelectedPlace,
-    log: document.querySelector(selectors.logOptionInput).checked,
-    dydx: document.querySelector(selectors.dydxOptionInput).checked
-  }
-}
-
-const colorway = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b, #e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-
+};
